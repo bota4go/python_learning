7954e34123a1eff63b18ae5a9779fb0f03b3ad34
bota4go <108249054+bota4go@users.noreply.github.com>
2022-08-13 20:37:19 +1000

    and of the day 1308

--- Ch 1/bota4go_cars.py
+++ Ch 1/bota4go_cars.py
@@ -3,30 +3,52 @@
 
 from datetime import date
 import sys
+from tkinter.tix import NoteBook
 from termcolor import colored, cprint
 
 # TODO: create a basic class
 class Car:
-    def __init__(self, brand, year, color, priceUSD):
+    def __init__(self, brand, year, color, price):
             self.brand = brand
             self.year = year
+            self.__PriceYear = 2020
             self.color = color
-            self.priceUSD = priceUSD
-            
+            self.price = price
+    
+    #calculate age        
     def age(self):
         return date.today().year-self.year
 
+    #return or calculate price
+    def getprice(self):
+        if hasattr(self, "_CorrectionPercent"):
+            self.price = self.price * self._CorrectionPercent
+            return self.price
+        else:
+            return self.price
 
-def onscreen(a):
-  message = colored('{} it is a brand', 'magenta')
-  print(message.format(a.brand), end=' ')
-  message = colored('and price is {}', 'yellow')
-  print(message.format(a.priceUSD), end=colored('$ ','yellow'))
-  message = colored('and age is {}', 'green')
-  print(message.format(a.age()), end=' ')
-  message = colored('and {} it is its color', 'red')
-  print(message.format(a.color))
-  return 0
+    #set correction 
+    def setnewprice(self, CorrectionPercent, CorrectionYear):
+        self._CorrectionPercent = CorrectionPercent
+        self._CorrectionYear = CorrectionYear
+
+    def onscreen(self):
+        message = colored('{} it is a brand', 'magenta')
+        print(message.format(self.brand), end=' ')
+        message = colored('and price is {}', 'yellow')
+        print(message.format(self.getprice()), end=colored('$ ','yellow'))
+        message = colored('and age is {}', 'green')
+        print(message.format(self.age()), end=' ')
+        message = colored('and {} it is its color', 'red')
+        print(message.format(self.color))
+        return 0
+
+
+class Notebook:
+    def __init__(self, Brand, year, price):
+        self.Brand = Brand
+        self.year = year
+        self.price = price
 
 
 
@@ -35,27 +57,29 @@ b1 = Car("BMW", 1986, "red", 10000)
 b2 = Car("Audi", 2007, "blue", 25000)
 b3 = Car("Mercedes", 2006, "light-grey", 5000)
 
-onscreen(b1)
-onscreen(b2)
-onscreen(b3)
-
-
-
-# print(b1.brand, b1.year, b1.color, b1.priceUSD, "USD")
-# print(b2.brand, b2.year, b2.color, b2.priceUSD, "USD")
-# print(b3.brand, b3.year, b3.color, b3.priceUSD, "USD")
-
-# message = '{} it is a brand'
-# print(message.format(b1.brand), end=' ')
-# message = 'and price is {}'
-# print(message.format(b1.priceUSD))
-
-# print(b1.age())
-# print(b2.age())
-# print(b3.age())
-
-
-
-
-
 # TODO: print the class and property
+b1.onscreen()
+b2.onscreen()
+b3.onscreen()
+
+b1.setnewprice(0.5, 2022)
+message = colored ('Set new price','blue', attrs=['blink', 'reverse'])
+print (message)
+b1.onscreen()
+
+print ("print secret", b1._Car__PriceYear)
+
+a1 = Notebook("Fujitsu", 2015, 1000)
+a2 = Notebook("MAC", 2022, 3500)
+
+print(type(b1))
+print(type(a1))
+
+print((type(a1)) == type(b1))
+print(isinstance(a1,Car))
+print(isinstance(b1,Car))
+
+
+
+
+
